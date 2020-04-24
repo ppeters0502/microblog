812537f9930fb530877252910967d956d980bb1a
Pat <pspeters93@gmail.com>
2020-04-23 19:46:56 -0500

    added requirements and started profile work

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import login
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -15,6 +16,12 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'http://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size
+        )
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
